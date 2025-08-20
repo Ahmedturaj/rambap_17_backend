0c006322b501cee392cbbe713a795c94e176c6b6
Ahmedturaj <ahmedturaj12@gmail.com>
2025-08-20 15:41:37 +0600

    dashboard api

--- src/entities/admin/admin.routes.js
+++ src/entities/admin/admin.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { adminMiddleware, verifyToken } from '../../core/middlewares/authMiddleware.js';
-import { updateUserDecisionController } from './admin.controller.js';
+import { getAllUsersControllerByAdmin, updateUserDecisionController } from './admin.controller.js';
 
 
 const router = express.Router();
 
-
+router.get("/all-users", verifyToken, adminMiddleware, getAllUsersControllerByAdmin);
 router.put("/:id/decision", verifyToken, adminMiddleware, updateUserDecisionController);
 
 
