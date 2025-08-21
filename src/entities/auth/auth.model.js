4e2eda6cf6a44877506fd50c557030bd18674b09
Ahmedturaj <ahmedturaj12@gmail.com>
2025-08-21 11:51:15 +0600

    removed name from models

--- src/entities/auth/auth.model.js
+++ src/entities/auth/auth.model.js
@@ -52,7 +52,6 @@ const decisionSchema = new mongoose.Schema({
 // ---------- USER SCHEMA ----------
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   password: { type: String, required: true },
